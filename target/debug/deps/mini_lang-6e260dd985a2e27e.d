4d806ab549f93dd46428ca5b94f1203eb6d896fc
kissy <221560843+KarlJPN@users.noreply.github.com>
2026-03-02 19:45:45 +0900

    fix: .gitignoreを修正しtarget/を除外

--- target/debug/deps/mini_lang-6e260dd985a2e27e.d
+++
@@ -1,5 +0,0 @@
-/Users/k-mori/Desktop/dev/mini-lang/target/debug/deps/mini_lang-6e260dd985a2e27e.d: src/main.rs
-
-/Users/k-mori/Desktop/dev/mini-lang/target/debug/deps/libmini_lang-6e260dd985a2e27e.rmeta: src/main.rs
-
-src/main.rs:
